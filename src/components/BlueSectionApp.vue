648254f31a9552e3973a9a0f458c535dd5568f4d
clmb00 <paolo.colombo2000@gmail.com>
2022-11-15 17:51:15 +0100

    blue bar start

--- src/components/BlueSectionApp.vue
+++ src/components/BlueSectionApp.vue
@@ -1,23 +1,74 @@
 <script>
 
 export default{
-  name: 'BlueSectionApp'
+  name: 'BlueSectionApp',
+  data(){
+    return{
+      collection: [
+        {
+          name: 'Digital Comics',
+          src: 'buy-comics-digital-comics.png'
+        }, {
+          name: 'Dc Merchandise',
+          src: 'buy-comics-merchandise.png'
+        }, {
+          name: 'Subscription',
+          src: 'buy-comics-subscriptions.png'
+        }, {
+          name: 'Comics Shop Locator',
+          src: 'buy-comics-shop-locator.png'
+        }, {
+          name: 'Dc Power Visa',
+          src: 'buy-dc-power-visa.svg'
+        }
+      ]
+    }
+  },
+  methods:{
+    getPathImage(imageName){
+      return new URL(`../assets/img/${imageName}`, import.meta.url).href
+    }
+  }
 }
 
 </script>
 
 <template>
   <section>
+    <div class="container">
+      <div v-for="(item, index) in collection" :key="index">
+        <img :src="getPathImage(item.src)" :alt="item.name">
+        <span>{{item.name.toUpperCase()}}</span>
+      </div>
+    </div>
   </section>
 </template>
 
 <style lang="scss" scoped>
 
 @use '../styles/partials/variables' as *;
+@use '../styles/partials/mixin' as *;
 
 section{
   background-color: $blue;
   color: white;
-  height: 155px;
+  .container{
+    height: 155px;
+    line-height: 155px;
+    @include flex('both');
+    justify-content: space-evenly;
+  }
+  div{
+    img{
+      height: 50px;
+      aspect-ratio: 1/1;
+      margin-right: 8px;
+      vertical-align: middle;
+    }
+    span{
+      font-size: .8rem;
+      white-space: nowrap;
+    }
+  }
 }
 </style>
