82e06b8fe638d5a5beef8d0b74fb2f6b48703496
clmb00 <paolo.colombo2000@gmail.com>
2022-11-15 16:41:20 +0100

    main and blue

--- src/components/BlueSectionApp.vue
+++ src/components/BlueSectionApp.vue
@@ -7,9 +7,17 @@ export default{
 </script>
 
 <template>
-  B
+  <section>
+  </section>
 </template>
 
 <style lang="scss" scoped>
 
+@use '../styles/partials/variables' as *;
+
+section{
+  background-color: $blue;
+  color: white;
+  height: 155px;
+}
 </style>
--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -7,9 +7,27 @@ export default{
 </script>
 
 <template>
-  M
+  <main>
+    <div class="container">
+      <p>--> Content goes here &lt;--</p>
+    </div>
+  </main>
 </template>
 
 <style lang="scss" scoped>
+
+@use '../styles/partials/variables' as *;
+
+main{
+  background-color: $black;
+  color: white;
+  // debug
+  height: 140px;
+  line-height: 140px;
+
+  p{
+    font-size: 1.8rem;
+  }
+}
 
 </style>
